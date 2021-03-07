36630074c1f96df8fe3c7d8f486a0f54d5220f8e
yuki ueno <yuki.weaver1204@gmail.com>
2021-03-07 18:21:33 +0900

    定数クラス作成

--- resources/views/restaurants/index.blade.php
+++ resources/views/restaurants/index.blade.php
@@ -94,3 +94,20 @@
     </div>
   </div>
 @endsection
+
+<script>
+successCallback= function(position)
+{
+  alert('緯度：' + position.coords.latitude);
+  alert('経度：' + position.coords.longitude);
+}
+errorCallback = function(error) {}
+
+if (navigator.geolocation) {
+  /* Geolocation APIを利用できる環境向けの処理 */
+  var navigator = navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  console.log(navigator);
+} else {
+  /* Geolocation APIを利用できない環境向けの処理 */
+}
+</script>
